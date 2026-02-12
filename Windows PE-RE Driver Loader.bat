50c00842f2226ae302f983468b1d2b2a955190b0
Yonatan Israeli <yonatanreuvenisraeli@gmail.com>
2026-02-11 16:00:34 -0800

    Update Windows PE-RE Driver Loader.bat

--- Windows PE-RE Driver Loader.bat
+++ Windows PE-RE Driver Loader.bat
@@ -2,16 +2,16 @@
 title Windows PE/RE Driver Loader
 setlocal
 echo Program Name: Windows PE/RE Driver Loader
-echo Version: 1.0.5
+echo Version: 1.0.6
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
 echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli
 "%windir%\System32\net.exe" user > nul 2>&1
-if "%errorlevel%"=="0" goto "NotInWindowsPreinstallationEnvironmentWindowsRecoveryEnvironment"
+if "%errorlevel%"=="0" goto "NotPERE"
 goto "Start"
 
-:"NotInWindowsPreinstallationEnvironmentWindowsRecoveryEnvironment"
+:"NotPERE"
 echo.
 echo You are not in Windows Preinstallation Environment or Windows Recovery Environment! You must run this batch file in Windows Preinstallation Environment or Windows Recovery Environment. Press any key to close this batch file.
 pause > nul 2>&1
